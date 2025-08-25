19d9b57d7f96d661d4fa1a5e31b1f9a244be17c7
pr-iti <priti90066@gmail.com>
2025-08-25 19:01:22 +0530

    this is new repo for backend projects and fast learning

---
+++ app.js
@@ -0,0 +1,18 @@
+//  const sum = require('./sum');
+
+ import { calculateSum } from './sum.js';
+import { x } from './sum.js';
+console.log(x);
+
+var namaste = "namaste node js";
+
+// var a =30;
+// var b = 40;
+
+// console.log(a+b);
+// console.log(namaste);
+// console.log(global);
+// console.log(this);
+calculateSum(3,6);
+// console.log(globalThis == this);
+// console.log(globalThis == global);
---
+++ sum.js
@@ -0,0 +1,7 @@
+//  modules protect their variables and function
+export var x = "!heyy, welcome to calculations";
+
+export function calculateSum (a,b){
+    return console.log(a+b);
+}
+// module.exports = { calculateSum : calculateSum};
