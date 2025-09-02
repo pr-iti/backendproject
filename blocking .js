d830b5527b200f05ca2163b9326e43a4bbcb532f
pr-iti <priti90066@gmail.com>
2025-09-02 19:28:13 +0530

    blocking i/o

---
+++ blocking .js
@@ -0,0 +1,24 @@
+const crypto = require('node:crypto');
+var a = 293;
+var b = 342;
+
+//password based key derivative func
+pbkdf2Sync("password","salt",5000,50,"asd123",(err,key) => {
+    console.log("1st password generated")
+});
+pbkdf2("password","salt",5000,50,"asd123",(err,key) => {
+    console.log("password generated")
+});
+
+ function multiply (x ,y ){
+    return x* y;
+ }
+
+ const prod = multiply(a,b);
+
+ console.log("product is evaluate first :-" ,prod);
+
+ module . exports = {
+    multiply,
+    prod
+ };
