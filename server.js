8012a260a47a86fa0a60677958746899bae668da
pr-iti <priti90066@gmail.com>
2025-09-08 10:29:37 +0530

    server created

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('node:http');
 
 const server = http.createServer(function (req, res) {
-    res.end("welcome to the server");
+   
+    if( req.url === "/getsecretdata"){
+        res.end("herre is my secret data -> universe alignment ");
+    }
+     res.end("welcome to the server");
 });
 
 server .listen(3000); 
